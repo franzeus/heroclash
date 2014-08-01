6b7fa1b374d980cd835575880e243f090834a9c7
Francois <webarbeit@gmail.com>
2014-08-01 20:55:33 +0200

    Some doc for the readers

--- js/game.js
+++ js/game.js
@@ -1,9 +1,13 @@
 var Game = {
-
+  // @var {Array} groups
   groups : [],
+  // @var {Array} deadGroups
   deadGroups: [],
+  // @var {Number} currentGroupIndex
   currentGroupIndex: 0,
+  // @var {Number} rounds
   rounds: 0,
+  // @const {Number} NEXT_ROUND_DELAY
   NEXT_ROUND_DELAY: 100,
 
   addGroup : function(group) {
@@ -11,6 +15,7 @@ var Game = {
       throw 'YUNO give me group object?!';
     }
     this.groups.push(group);
+    return this;
   },
 
   start : function() {
@@ -27,13 +32,18 @@ var Game = {
 
   gameOver : function(winnerGroup) {
     console.log("WINNER", winnerGroup.id);
+    console.log("Rounds", this.rounds);
   },
 
+  /**
+   * Let current group attack another group until there is only one group left
+   */
   playRound : function() {
     var self = this;
     var attackingGroup = this.groups[this.currentGroupIndex];
     var groupToAttack = this.getGroupToAttack(attackingGroup);
     console.log('-----------------------');
+    self.rounds++;
     console.log(attackingGroup.id, 'attacks', groupToAttack.id);
     groupToAttack.attackedBy(attackingGroup, this.attackWillKillNumOfCreatures());
 
@@ -49,15 +59,17 @@ var Game = {
     // Next round
     } else {
       setTimeout(function() {
-        self.rounds++;
         self.increaseGroupIndex.call(self);
         self.playRound.call(self);
       }, this.NEXT_ROUND_DELAY);
     }
   },
 
-  // Lets get a random group to attack, but not the currently attacking group
-  // Todo: would actually be funny :D
+  /**
+   * Returns a random group of the groups list, which will be attacked by current group
+   * @param {Object} attackingGroup - The current group which attacks
+   * @return {Object}
+   */
   getGroupToAttack : function(attackingGroup) {
     var groupToAttack = null;
     do {
@@ -66,12 +78,18 @@ var Game = {
     return groupToAttack;
   },
 
+  /**
+  * @return {Number}
+  */
   attackWillKillNumOfCreatures : function() {
     var creaturesToKill = getRandomInt(0, 1);
     //TODO: non deterministic code
     return creaturesToKill;
   },
 
+  /**
+  * Increases the current group index
+  */
   increaseGroupIndex : function() {
     var newIndex = this.currentGroupIndex + 1;
     if (newIndex >= this.groups.length) {
@@ -85,6 +103,10 @@ var Game = {
     }
   },
 
+  /**
+  * Adds a group to the dead groups list
+  * @param {Object} group - The group to add
+  */
   addDeadGroup : function(group) {
     if (this.isGroupDead(group)) {
       console.warn('This group is actually already dead', group.id);
@@ -94,17 +116,27 @@ var Game = {
     this.deadGroups.push(group.id);
   },
 
+  /**
+   * Returns true of group can not attack or be attacked
+   * @param {Object} group - The group to check
+   * @return {Boolean}
+   */
   isGroupDead : function(group) {
     return this.deadGroups.indexOf(group.id) > -1;
   },
 
+  /**
+   * Returns index of the groups list of a groupd by the group id property
+   * @param {String} id - The group id
+   * @return {Number|Null}
+   */
   getGroupIndexById : function(id) {
     for (var i = 0; i < this.groups.length; i++) {
       if (this.groups[i].id === id) {
         return i;
       }
     }
-    return i;
+    return null;
   }
 
 
